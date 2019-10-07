14dc7187804ad1376b3ed7292ebf6e4229e01255
Dolores C <doloreschu900728@gmail.com>
2019-10-07 13:41:19 -0400

    Commit

--- zchu_5.R
+++ zchu_5.R
@@ -1,11 +1,22 @@
 library(lpSolveAPI)
+
+#IP formulation for question #1
+z<-read.lp("zchu_5c.lp")
+solve(z)
+get.objective(z)
+get.variables(z)
+
+#IP formulation for question #2 part 1)
 x<-read.lp("zchu_5a.lp")
 solve(x)
 get.objective(x)
 get.variables(x)
 x
 
+#IP formulation for question #2 part 2)
 y<-read.lp("zchu_5b.lp")
 solve(y)
 get.objective(y)
 get.variables(y)
+
+
